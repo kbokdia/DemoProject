db3f5b0975faee04f53b322be56aa0ed2a7f806f
Kamlesh Bokdia <kbokdia@gmail.com>
2016-01-22 14:01:47 +0530

    Gallery: - SaveGallery Function initialized

--- gallery/Gallery.php
+++ gallery/Gallery.php
@@ -13,7 +13,28 @@ use Project\base\BaseClass;
 
 class Gallery extends BaseClass
 {
+    var $galleryName;
+    var $imageCount;
+    var $coverImagePath;
+
     function __construct(){
         parent::__construct();
+    }
+
+    //save gallery info
+    function saveGalleryInfo(){
+        //create insert query and save to db and on success return true else false
+    }
+
+    //generate gallery ID
+    function generateGalleryId(){
+        $galleryCode = 1001;
+        $sql = "SELECT MAX(Id) AS 'galleryCode' FROM Gallery";
+        if($result = $this->mysqli->query($sql)){
+            $galleryCode = intval($result->fetch_assoc()['galleryCode']);
+            $galleryCode = (($galleryCode == 0) ? 1001 : $galleryCode + 1);
+        }
+
+        return $galleryCode;
     }
 }
