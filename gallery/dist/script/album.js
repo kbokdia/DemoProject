410d383467a2741e83afec357bf382d58c208e48
Kamlesh Bokdia <kbokdia@gmail.com>
2016-02-01 16:54:47 +0530

    Gallery: - delete image functionality  initiated.

---
+++ gallery/dist/script/album.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kbokdia on 01/02/16.
+ */
+pageAlbum = {
+    baseURL : "controller.php",
+
+    //Implement delete image functionality
+    deleteImage : function(galleryId, imageId){
+        //Request TYPE : DI
+        //Refer deleteGallery function in script.js
+    }
+};
