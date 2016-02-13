2463a1aaf78c869601d1d1b4068a69e370d2be05
Kamlesh Bokdia <kbokdia@gmail.com>
2016-02-13 14:58:44 +0530

    RSVP class updated

--- events/rsvp/Rsvp.php
+++ events/rsvp/Rsvp.php
@@ -15,39 +15,33 @@ class Rsvp extends BaseClass
 {
     //Todo-Ambuj: You have to implement the whole class
     //Declare the variables.
-    var $regId;
     var $eventId;
     var $memberId;
     var $self;
     var $spouse;
     var $children;
     var $guest;
-    var $timeStamp;
 
-    function __construct()
-    {
+    function __construct(){
         parent::__construct();
     }
 
     //Add new RSVP for an event.
-    function addRsvp($eventId)
-    {
-        $sql = "INSERT into Rsvp(RegId,EventId,MemberId,Self,Spouse,Children,Guest,Timestamp) values($this->regId,$eventId,$this->memberId,$this->self,$this->spouse,$this->children,$this->guest,'$this->timeStamp')";
-        if($result = $this->mysqli->query($sql))
-        {
+    function addRsvp($regId,$eventId,$memberId){
+        $sql = "INSERT INTO Rsvp(RegId,EventId,MemberId,Self,Spouse,Children,Guest) VALUES($regId,$eventId,$memberId,$this->self,$this->spouse,$this->children,$this->guest)";
+
+        if($result = $this->mysqli->query($sql)) {
             $response = BaseClass::createResponse(1,"RSVP added..");
         }
-        else
-        {
-            $response = BaseClass::createResponse(0,"RSVP not added..");
+        else {
+            $response = BaseClass::createResponse(0,$this->mysqli->error);
         }
         return $response;
     }
 
     //Get existing RSVP.
-    function getRsvp($eventId)
-    {
-        $sql = "SELECT Rsvp.RegId, Rsvp.EventId, Rsvp.MemberId, Rsvp.Self, Rsvp.Spouse, Rsvp.Children, Rsvp.Guest, Rsvp.Timestamp FROM Rsvp WHERE Rsvp.EventId = $eventId";
+    function getRsvp($eventId){
+        $sql = "SELECT Rsvp.RegId, Rsvp.EventId, Rsvp.MemberId, Rsvp.Self, Rsvp.Spouse, Rsvp.Children, Rsvp.Guest, Rsvp.Timestamp FROM Rsvp WHERE Rsvp.RegId = $this->regId AND Rsvp.EventId = $eventId";
         $result = $this->mysqli->query($sql);
         if ($result) {
             $i = 0;
@@ -63,36 +57,27 @@ class Rsvp extends BaseClass
     }
 
     //Delete a RSVP.
-    function deleteRsvp($eventId)
-    {
-        $sql="SELECT * FROM Rsvp WHERE EventId=$eventId";
-        if($result = $this->mysqli->query($sql))
-        {
-            $sql = "DELETE FROM Rsvp WHERE EventId = $eventId";
-            if($this->mysqli->query($sql)){
-                $response = BaseClass::createResponse(1,"Rsvp deleted");
-            }
-            else{
-                $response = BaseClass::createResponse(0,$this->mysqli->error);
-            }
+    function deleteMemberRsvp($eventId,$memberId){
+        $sql = "DELETE FROM Rsvp WHERE RegId = $this->regId AND EventId = $eventId AND Rsvp.MemberId = $memberId";
+
+        if($this->mysqli->query($sql)){
+            $response = BaseClass::createResponse(1,"Rsvp deleted");
         }
         else{
-            $response = BaseClass::createResponse(0,"Invalid request");
+            $response = BaseClass::createResponse(0,$this->mysqli->error);
         }
 
         return $response;
     }
 
     //Editing a RSVP.
-    function updateRsvp($eventId)
-    {
-        $sql = "UPDATE Rsvp SET Rsvp.MemberId=$this->memberId, Self=$this->self , Rsvp.Spouse=$this->spouse, Rsvp.Children = $this->children, Rsvp.Guest=$this->guest, Rsvp.Timestamp='$this->timeStamp' WHERE Rsvp.EventId=$eventId";
-        if($result = $this->mysqli->query($sql))
-        {
+    function updateRsvp($eventId, $memberId){
+        $sql = "UPDATE Rsvp SET Self=$this->self , Rsvp.Spouse=$this->spouse, Rsvp.Children = $this->children, Rsvp.Guest=$this->guest WHERE Rsvp.RegId = $this->regId AND Rsvp.EventId=$eventId AND Rsvp.MemberId=$memberId";
+
+        if($result = $this->mysqli->query($sql)) {
             $response = BaseClass::createResponse(1,"Rsvp updated..");
         }
-        else
-        {
+        else {
             $response = BaseClass::createResponse(0,"Rsvp not updated..");
         }
         return $response;
