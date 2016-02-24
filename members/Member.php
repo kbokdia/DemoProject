17e18531ba877d1524a6166bedb14dfe10b22d14
ambujaacool <ambujaacool@gmail.com>
2016-02-24 16:20:16 +0530

    All validations

--- members/Member.php
+++ members/Member.php
@@ -98,7 +98,6 @@ class Member extends BaseClass
     function updateMember($regId,$memberId)
     {
         $sql = "UPDATE Members SET Name=$this->name,GuardianName=$this->guardianName,DOB=$this->dob,Gender=$this->gender,Email=$this->email ,Email1=$this->email1 ,Mobile=$this->mobile ,Mobile1=$this->mobile1 ,Mobile2=$this->mobile2 ,BloodGroup=$this->bloodGroup,NativePlace=$this->nativePlace ,HomeAddress=$this->homeAddress ,HomeAreaCode= $this->homeAreaCode ,HomePincode=$this->homePincode ,HomeCityCode= $this->homeCityCode ,HomeStateCode= $this->homeStateCode ,HomePhone= $this->homePhone ,HomeCentrex= $this->homeCentrex ,Occupation= $this->occupation ,BusinessType= $this->businessType ,OfficeAddress= $this->officeAddress ,OfficeAreaCode= $this->officeAreaCode ,OfficePincode= $this->officePincode ,OfficeCityCode= $this->officeCityCode ,OfficeStateCode= $this->officeStateCode ,OfficePhone= $this->officePhone ,OfficeCentrex= $this->officeCentrex ,Food= $this->food ,Religion= $this->religion ,Hobbies= $this->hobbies ,Recognition= $this->recognition ,MembershipNo= $this->membershipNo ,MembershipType= $this->membershipType ,MemberJoiningDate= $this->memberJoiningDate ,HasPartner=$this->hasPartner, HasChildren=$this->hasChildren , Active=$this->active WHERE RegId=$regId AND MemberId=$memberId";
-        echo $sql;
         if($result = $this->mysqli->query($sql))
         {
             $response = BaseClass::createResponse(1,"Member details updated.");
